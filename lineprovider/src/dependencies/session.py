d52a0e878d082bad063cc73138c0e3e832eefc13
fJusk <piter.ruf@gmail.com>
2024-08-05 01:30:40 +0300

    add deps

--- lineprovider/src/dependencies/session.py
+++ lineprovider/src/dependencies/session.py
@@ -2,10 +2,10 @@
 from typing import AsyncGenerator
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from src.database.connection import ASYNC_SESSION
+from src.database.connection import SessionMaker
 
 
 async def get_session() -> AsyncGenerator[AsyncSession, None]:
     """Get async session."""
-    async with ASYNC_SESSION() as session:
+    async with SessionMaker() as session:
         yield session
